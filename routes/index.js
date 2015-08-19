501c420d7820a1cf987694f6d934fd9d0da5a154
Derek Vance <devance65@gmail.com>
2015-08-19 15:41:46 -0600

    base app set up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+router = express.Router(),
+mongo = require('../lib/mongo');
 
+//one function per route
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Comapny Management Tool' });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('new');
+});
+
+router.get('/show', function(req, res, next) {
+  res.render('show');
+});
+
+router.post('/new', function(req, res, next){
+  res.redirect('show');
+});
+
+router.get('/edit', function(req, res, next){
+  res.render('edit');
+});
+
+router.post('/edit', function(req, res, next){
+  res.redirect('show');
+});
+
+router.post('/delete', function (req, res, next) {
+  res.redirect('show');
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+router = express.Router(),
+db = require('monk')(process.env.HOST)
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
